64b00a8b4fd75094522a8963c3a47f666f895d89
CarlosCordoba96 <carloscordobaruiz@gmail.com>
2016-10-30 17:19:08 +0100

    Implementando clases

--- src/State.java
+++ src/State.java
@@ -3,15 +3,13 @@
 public class State {
 	private int n_rows;
 	private int n_cols;
-
+	private int [] cero;
 	public State(){
-		/*
-		 * El puzzle, el array de numeros o de imagenes?
-		 * n_rows & n_cols
-		 *position of the 0
-		 *METHODS:
-		 *isValid isGoal succesor		
-		 */
+		
 	}
-
+	/*
+	 * El puzzle, el array de numeros o de imagenes?
+	 *METHODS:
+	 *isValid isGoal succesor		
+	 */
 }
--- src/nodeTree.java
+++ src/nodeTree.java
@@ -4,8 +4,15 @@ public class nodeTree {
 	private State state;
 	private int partialCost;
 	private char action;
-	public nodeTree(){
+	public nodeTree(nodeTree parent, State state, int partialCost,char action){
+		this.parent=parent;
+		this.state=state;
+		this.partialCost=partialCost;
+		this.action=action;
 
 	}
+	public nodeTree(){
+		
 
+	}
 }
