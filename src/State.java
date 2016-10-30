e15b66898aa675aa67b61e7cec3c5551a222af9c
CarlosCordoba96 <carloscordobaruiz@gmail.com>
2016-10-30 13:10:11 +0100

    Clases creadas para trabajar en ellas

---
+++ src/State.java
@@ -0,0 +1,17 @@
+
+
+public class State {
+	private int n_rows;
+	private int n_cols;
+
+	public State(){
+		/*
+		 * El puzzle, el array de numeros o de imagenes?
+		 * n_rows & n_cols
+		 *position of the 0
+		 *METHODS:
+		 *isValid isGoal succesor		
+		 */
+	}
+
+}
---
+++ src/nodeTree.java
@@ -0,0 +1,11 @@
+
+public class nodeTree {
+	private nodeTree parent;
+	private State state;
+	private int partialCost;
+	private char action;
+	public nodeTree(){
+
+	}
+
+}
