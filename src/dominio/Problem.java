d2b22c36868b58084a64aa513b5e39f82004ab2a
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2016-11-29 19:33:20 +0100

    Commit

--- src/dominio/Problem.java
+++ src/dominio/Problem.java
@@ -9,57 +9,59 @@ public class Problem {
 
 	private StateSpace goalState;
 	private StateSpace initialState;
+	private double time;
+	private int visitednodes=0;
 	
 	public Problem(StateSpace goalstate, StateSpace initialState){
 		this.goalState=goalstate;
 		this.initialState=initialState;
 	}
-	private Queue<Character> acSolve(StateSpace s, String strat, int maxdepth){
-		Queue<Character> q = new PriorityQueue<Character>();
-		Queue<nodeTree> qbuff = new PriorityQueue<nodeTree>(new Comparator<nodeTree>() {
-			public int compare(nodeTree e1, nodeTree e2) {
-				if(e1.getValue() > e2.getValue())
-					return 1;
-				
-				else if(e1.getValue() < e2.getValue())
-					return -1;
-				
-				else
-					return 0;
-			}
-		});
+	
+	private Queue<Character> acSolve(String strat, int maxdepth){
+		double stime=System.currentTimeMillis();
+		Queue<Character> rtrn = new PriorityQueue<Character>();
+		Queue<nodeTree> qbuff = createFrontier();
 		
-		nodeTree initialNode = new nodeTree(s);
-		Stack<StateSpace> sbuff;
+		nodeTree initialNode = new nodeTree(this.initialState);
+		Queue<StateSpace> sbuff;
 		StateSpace ssbuff;
 		nodeTree actualNode = null;
-		qbuff.add(initialNode);
-		nodeTree newnode = null;
-		
 		boolean sol = false;
+		nodeTree newnode =null;
 		
-		while(!sol && !qbuff.isEmpty()){
-			actualNode = qbuff.poll();
+		insertFrontier(initialNode,qbuff);
+		
+		while(!sol && !frontierIsEmpty(qbuff)){
+			actualNode = removeFirstFrontier(qbuff);
+			visitednodes++;
 			if(actualNode.getStateSpace().isGoal(goalState)){
 				sol = true;
 			}else{
 				sbuff = actualNode.getStateSpace().succesor();
+				printarray(actualNode.getStateSpace().getPuzzle());
 				while(!sbuff.isEmpty()){
-					ssbuff = sbuff.pop();
+					ssbuff = sbuff.poll();
 					try {
 						newnode = new nodeTree(actualNode,ssbuff,ssbuff.action,strat,maxdepth);
+						
 					} catch (MdepthException e) {
-						newnode = null;
-						continue;
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-					if (newnode != null)
-					qbuff.add(newnode);
+					if(newnode !=null){
+						insertFrontier(newnode,qbuff);
+					}
 				}
 			}
 		}
 		if(sol){
-			actualNode.getPath(q);
-			return q;
+			
+			double endtime=System.currentTimeMillis();
+			time=(endtime-stime)/1000;
+			System.out.println("Hay solución");
+			actualNode.getPath(rtrn);
+			System.out.println("He salido");
+			return rtrn;
 		}
 		return null;
 	}
@@ -69,10 +71,10 @@ public Queue<Character> search(StateSpace s, String strat,int maxdepth, int init
 		int actualdepth = initialdepth;
 		Queue<Character> q = new PriorityQueue<Character>();
 		while(q.isEmpty() && actualdepth <= maxdepth){
-			q = acSolve(s,strat,actualdepth);
+			q = acSolve(strat,actualdepth);
 			actualdepth += initialdepth;
 		}
-		return null;
+		return q;
 	}
 	
 	
@@ -104,5 +106,13 @@ public Queue<Character> search(StateSpace s, String strat,int maxdepth, int init
 		return frontier.isEmpty();
 		}
 
+	public static void printarray(int [][] a){
+		for(int i = 0; i < a.length ; i ++){
+			for(int j = 0 ; j < a[i].length ; j ++){
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.print("\n");
+		}
+	}
 
 }
