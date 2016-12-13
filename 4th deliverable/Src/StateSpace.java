c8a079786c167da45a1c47d4fb291d868279766a
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2016-12-13 13:59:09 +0100

    4th deliverable

---
+++ 4th deliverable/Src/StateSpace.java
@@ -0,0 +1,208 @@
+package dominio;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class StateSpace {
+	private static int n_rows;
+	private static int n_cols;
+	private int icero;
+	private int jcero;
+	private final int [][] puzzle;
+	char action;
+	private int [][] goalpuzzle;
+	
+	public StateSpace(int rows, int cols, int xcero, int ycero, int [][] puzzle,int[][]goal){
+		this.n_rows=rows;
+		this.n_cols=cols;
+		this.icero = xcero;
+		this.jcero = ycero;
+		this.puzzle=puzzle;
+		this.goalpuzzle=goal;
+	}
+	public StateSpace(int rows, int cols, int xcero, int ycero, int [][] puzzle,char action,int[][]goal) {
+		this.n_rows=rows;
+		this.n_cols=cols;
+		this.puzzle=puzzle;
+		this.action=action;
+		this.icero = xcero;
+		this.jcero = ycero;
+		this.goalpuzzle=goal;
+	}
+
+
+	public  boolean isValid(char c){
+		boolean sol=false;
+		switch(c){
+		case 'u':
+			if(ispossUp()){
+				sol=true;
+			}
+			break;
+		case 'd':
+			if(ispossDown()){
+				sol=true;
+			}
+			break;
+		case 'l':
+			if(ispossLeft()){
+				sol=true;
+			}
+			break;
+		case 'r':
+			if(ispossRight()){
+				sol=true;
+			}
+			break;
+		}
+		return sol;
+	}
+
+	public  Stack<Character> posiblemov(){
+		Stack<Character> movements = new Stack<Character>();
+		if(isValid('u')){
+			movements.push('u');
+		}
+		if(isValid('d')){
+			movements.push('d');
+		}
+		if(isValid('l')){
+			movements.push('l');
+		}
+		if(isValid('r')){
+			movements.push('r');
+		}
+		return movements;
+	}
+
+	public void generatesuccesor(Queue<StateSpace> succesors,char mov, int[][] p){
+		move(mov,p);
+		StateSpace StateSpacenew = null;
+		switch(mov){
+		case 'u':
+			StateSpacenew=new StateSpace(n_rows,n_cols,this.icero++, this.jcero,p,mov,goalpuzzle);
+			break;
+		case 'd':
+			StateSpacenew=new StateSpace(n_rows,n_cols,this.icero--, this.jcero,p,mov,goalpuzzle);
+			break;
+		case 'l':
+			StateSpacenew=new StateSpace(n_rows,n_cols,this.icero, this.jcero++,p,mov,goalpuzzle);
+			break;
+		case 'r':
+			StateSpacenew=new StateSpace(n_rows,n_cols,this.icero, this.jcero--,p,mov,goalpuzzle);
+			break;
+		default:
+			StateSpacenew=new StateSpace(n_rows,n_cols,this.icero, this.jcero,p,mov,goalpuzzle);
+			break;
+		}
+		succesors.add(StateSpacenew);
+	}
+	public Queue<StateSpace> succesor(){
+		Queue<StateSpace> s = new LinkedList<StateSpace>();
+		Stack<Character> posmov=posiblemov();
+		while(!posmov.isEmpty()){
+			generatesuccesor(s,posmov.pop(),copyarr(this.puzzle));
+		}
+
+		return s;
+	}
+	public int[][] getPuzzle() {
+		return puzzle;
+	}
+	private void move(char c, int [][]arr){
+
+		switch(c){
+		case 'u':
+			arr[icero][jcero] = arr[icero-1][jcero];
+			arr[icero-1][jcero] = 0;
+			this.icero--;
+			break;
+		case 'd':
+			arr[icero][jcero] = arr[icero+1][jcero];
+			arr[icero+1][jcero] = 0;
+			this.icero++;
+			break;
+		case 'l':
+			arr[icero][jcero] = arr[icero][jcero-1];
+			arr[icero][jcero-1] = 0;
+			this.jcero--;
+			break;
+		case 'r':
+			arr[icero][jcero] = arr[icero][jcero+1];
+			arr[icero][jcero+1] = 0;
+			this.jcero++;
+			break;
+		}
+
+	}
+	public  boolean ispossUp(){
+		boolean posible=false;
+		if(icero!=0){
+			posible=true;
+		}
+		return posible;
+	}
+	public  boolean ispossDown(){
+		boolean posible=false;
+		if(icero!=n_rows-1){
+			posible=true;
+		}
+		return posible;
+	}
+	public  boolean ispossLeft(){
+		boolean posible=false;
+		if(jcero!=0){
+			posible=true;
+		}
+		return posible;
+	}
+	public  boolean ispossRight(){
+		boolean posible=false;
+		if(jcero !=n_cols-1){
+			posible=true;
+		}
+		return posible;
+	}
+	public static void printarray(int[][] array){
+
+		for(int i = 0; i<array.length;i++){
+			for(int j = 0; j<array[0].length;j++){
+
+				System.out.print(array[i][j]+" ");
+			}
+			System.out.print("\n");
+		}
+	}
+	public static int [] [] copyarr(int [][]array){
+		int [] [] a = new int[n_rows][n_cols];
+		for(int i=0;i<array.length;i++){
+			for(int j=0;j<array[i].length;j++){
+				a[i][j]=array[i][j];
+			}
+		}
+		return a;
+	}
+	public boolean isGoal( ) {
+		boolean goal = false;
+
+		if (Arrays.deepEquals(puzzle,goalpuzzle))
+			goal = true;
+
+		return goal;
+	}
+	public int heuristic(){
+		int h=0;
+		for(int i=0;i<puzzle.length;i++){
+			for(int j=0;j<puzzle[i].length;j++){
+				if(puzzle[i][j] != goalpuzzle[i][j]){
+					h++;
+				}
+			}
+		}
+		return h;
+	}
+}
---
+++ 4th deliverable/Src/nodeTree.java
@@ -0,0 +1,96 @@
+package dominio;
+
+import java.util.Queue;
+
+public class nodeTree {
+	private nodeTree parent;
+    StateSpace StateSpace;
+	private int partialCost;
+	private char action;
+	private int depth;
+	private float value;
+	
+	public nodeTree(nodeTree parent, StateSpace StateSpace, char action,String strat,int Mdepth) throws MdepthException{
+		this.parent=parent;
+		this.StateSpace=StateSpace;
+		this.action=action;
+		this.partialCost= parent.getPartialCost()+1;
+		this.depth=parent.getDepth()+1;
+		if (depth > Mdepth) {
+			throw new MdepthException();
+		}
+		
+		if(strat.equals("BFS")){
+			
+			this.value=this.depth;
+			
+		}else if(strat.equals("DFS") ||strat.equals("DLS")|| strat.equals("IDS") ){
+			
+			this.value=-this.depth;
+			
+		}else if(strat.equals("UCS")){
+			
+			this.value=this.partialCost;
+		}else if(strat.equals("A*")){
+			value=partialCost +StateSpace.heuristic();// La heuristica tenemos que implementarlo, puede ser el nº de tiles descolocadas
+		}
+	}
+
+	public nodeTree(StateSpace StateSpace){
+		this.parent=null;
+		this.StateSpace=StateSpace;
+		this.partialCost=0;
+		this.action=' ';	
+		this.depth=0;
+		this.value =0;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+	public nodeTree getParent() {
+		return parent;
+	}
+	public void setParent(nodeTree parent) {
+		this.parent = parent;
+	}
+	public StateSpace getStateSpace() {
+		return StateSpace;
+	}
+	public void setStateSpace(StateSpace StateSpace) {
+		this.StateSpace = StateSpace;
+	}
+	public int getPartialCost() {
+		return (int) partialCost;
+	}
+	public void setPartialCost(int partialCost) {
+		this.partialCost = partialCost;
+	}
+	public char getAction() {
+		return action;
+	}
+	
+	public void setAction(char action) {
+		this.action = action;
+	}
+	
+	public float getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public void getPath(Queue<Character> q){
+		if(!(this.parent == null)){
+			this.parent.getPath(q);
+		}
+		if(this.action != ' ')
+			q.add(this.action);
+	}
+	
+}
